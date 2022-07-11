162fecba402b65b52ee6810213b94be67b47396b
Lahanava <107791508+Lahanava@users.noreply.github.com>
2022-07-11 11:41:40 +0700

    Add files via upload

---
+++ NguyenDucCuong/taocanhan.java
@@ -0,0 +1,332 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package nganhang;
+
+import DBCN.ConectDB;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+import java.lang.String;
+import DBCN.DB_hskh;
+
+/**
+ *
+ * @author Adminpc
+ */
+public class taocanhan extends javax.swing.JFrame {
+
+    public taocanhan() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.setTitle("Hệ thống quản lý Ngân hàng HNC");
+
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        txtCNSDT = new javax.swing.JTextField();
+        txtCNTen = new javax.swing.JTextField();
+        txtCNHM = new javax.swing.JTextField();
+        txtCNCMND = new javax.swing.JTextField();
+        txtCNDC = new javax.swing.JTextField();
+        btnCnMo = new javax.swing.JButton();
+        DateT1 = new com.toedter.calendar.JDateChooser();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel1.setText("Mở hồ sơ cho cá nhân");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel2.setText("Số CMND");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel3.setText("Họ tên");
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel4.setText("Địa chỉ");
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel5.setText("Số điện thoại");
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel6.setText("Ngày tạo hồ sơ");
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jButton1.setText("Quay lại trang chủ");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel7.setText("Hạn mức tín dụng");
+
+        jLabel8.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel8.setText("(VNĐ)");
+
+        txtCNSDT.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        txtCNTen.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        txtCNHM.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        txtCNCMND.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        txtCNDC.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        btnCnMo.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        btnCnMo.setText("Mở hồ sơ");
+        btnCnMo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCnMoActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel6)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel5)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel7))
+                                .addGap(30, 30, 30)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtCNCMND, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtCNTen, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtCNSDT, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtCNDC, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(txtCNHM, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(jLabel8))
+                                    .addComponent(DateT1, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButton1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(252, 252, 252)
+                        .addComponent(btnCnMo)))
+                .addContainerGap(73, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1)
+                .addGap(23, 23, 23)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtCNCMND, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtCNTen, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(txtCNSDT, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txtCNDC, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(24, 24, 24)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel6)
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel8)
+                            .addComponent(txtCNHM, javax.swing.GroupLayout.DEFAULT_SIZE, 33, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addComponent(btnCnMo, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(20, 20, 20))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(DateT1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                new frmHNC().setVisible(true);
+            }
+        });
+
+        setVisible(false);
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void btnCnMoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCnMoActionPerformed
+
+        if ((txtCNCMND.getText().equals("")) || (txtCNTen.getText().equals("")) || (txtCNSDT.getText().equals(""))
+                || (txtCNDC.getText().equals("")) || (txtCNHM.getText().equals(""))) {
+            JOptionPane.showMessageDialog(rootPane, "Nhập thiếu thông tin!!!");
+
+        } else {
+            String tx1 = txtCNCMND.getText();
+            boolean x1 = true;
+            for (int i = 0; i < tx1.length(); i++) {
+                if ((tx1.charAt(i) != '0') && (tx1.charAt(i) != '1') && (tx1.charAt(i) != '2') && (tx1.charAt(i) != '3')
+                        && (tx1.charAt(i) != '4') && (tx1.charAt(i) != '5') && (tx1.charAt(i) != '6') && (tx1.charAt(i) != '7')
+                        && (tx1.charAt(i) != '8') && (tx1.charAt(i) != '9')) {
+                    x1 = false;
+                }
+            }
+            if ((x1 == false) || ((tx1.length() != 12) && (tx1.length() != 8))) {
+                JOptionPane.showMessageDialog(rootPane, "Nhập sai số CMND!!!");
+            } else {
+                String tx2 = txtCNSDT.getText();
+                boolean x2 = true;
+                for (int i = 0; i < tx2.length(); i++) {
+                    if ((tx2.charAt(i) != '0') && (tx2.charAt(i) != '1') && (tx2.charAt(i) != '2') && (tx2.charAt(i) != '3')
+                            && (tx2.charAt(i) != '4') && (tx2.charAt(i) != '5') && (tx2.charAt(i) != '6') && (tx2.charAt(i) != '7')
+                            && (tx2.charAt(i) != '8') && (tx2.charAt(i) != '9')) {
+                        x2 = false;
+                    }
+                }
+                if (x2 == false) {
+                    JOptionPane.showMessageDialog(rootPane, "Nhập sai số điện thoại!!!");
+                } else {
+                    String tx3 = txtCNHM.getText();
+                    boolean x3 = true;
+                    for (int i = 0; i < tx3.length(); i++) {
+                        if ((tx3.charAt(i) != '0') && (tx3.charAt(i) != '1') && (tx3.charAt(i) != '2') && (tx3.charAt(i) != '3')
+                                && (tx3.charAt(i) != '4') && (tx3.charAt(i) != '5') && (tx3.charAt(i) != '6') && (tx3.charAt(i) != '7')
+                                && (tx3.charAt(i) != '8') && (tx3.charAt(i) != '9')) {
+                            x3 = false;
+                        }
+                    }
+                    if (x3 == false) {
+                        JOptionPane.showMessageDialog(rootPane, "Nhập sai hạn mức tín dụng!!!");
+                    } else {
+                        boolean xett = true;
+                        DB_hskh gg = new DB_hskh();
+                        for (hosokhachhang j : gg.layhoso()) {
+                            if (j.getMaHS().equals(tx1)) {
+                                xett = false;
+                                
+                            }
+                        }
+                        if (xett == false) {
+                            JOptionPane.showMessageDialog(rootPane, "Số CMND này đã tồn tại");
+                        } else {
+                            try {
+                                Connection cn = ConectDB.getConectDB();
+                                PreparedStatement ps = cn.prepareStatement("Insert into Hoso (MaHs, LoaiHS, Tendk, Sdt, Diachi, Hmtd, Ngay) Values (?,?,?,?,?,?,?)");
+                                ps.setString(1, txtCNCMND.getText());
+                                ps.setString(2, "Cá nhân");
+                                ps.setString(3, txtCNTen.getText());
+                                ps.setString(4, txtCNSDT.getText());
+                                ps.setString(5, txtCNDC.getText());
+                                long t5 = Long.parseLong(txtCNHM.getText());
+                                ps.setLong(6, t5);
+                                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                                String d = sdf.format(DateT1.getDate());
+                                ps.setString(7, d);
+
+                                int check = ps.executeUpdate();
+
+                                JOptionPane.showMessageDialog(rootPane, "Đã tạo thành công hồ sơ!");
+
+                            } catch (SQLException ex) {
+                                java.util.logging.Logger.getLogger(taocanhan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                            }
+
+                        }
+
+                    }
+                }
+            }
+        }
+    }//GEN-LAST:event_btnCnMoActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(taocanhan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(taocanhan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(taocanhan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(taocanhan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new taocanhan().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.toedter.calendar.JDateChooser DateT1;
+    private javax.swing.JButton btnCnMo;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JTextField txtCNCMND;
+    private javax.swing.JTextField txtCNDC;
+    private javax.swing.JTextField txtCNHM;
+    private javax.swing.JTextField txtCNSDT;
+    private javax.swing.JTextField txtCNTen;
+    // End of variables declaration//GEN-END:variables
+}
